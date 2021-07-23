5485a6433341f99a37605e6f58afae43253fa0c0
Cyril Deschamps <77438408+CyrilDlaPompote@users.noreply.github.com>
2021-07-23 15:19:31 +0200

    new functions and emulator

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
-const functions = require("firebase-functions");
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.createUserDocument = functions.auth.user().onCreate(async(user) => {
+  try {
+    await admin.firestore().collection('users').doc(user.uid).set({
+      credit: 0,
+      uid: user.uid,
+      posts: []
+    });
+  } catch(error) {
+    console.log(error)
+  }
+  
+  return true;
+});
+
+exports.createUserDocument = functions.firestore.document('posts/posts/{postType}/{docId}').onCreate(async(snap, context) => {
+  try {
+    const data = snap.data();
+    await admin.firestore().collection('users').doc(data.author_id).update({ posts: admin.firestore.FieldValue.arrayUnion({
+      title: data.title,
+      date: data.date,
+      thumbnail: data.evaluation[0].image
+    })});
+  } catch (error) {
+    console.log(error);
+  }
+  
+  return true;
+});
